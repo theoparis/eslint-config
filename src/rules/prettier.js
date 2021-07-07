b992dc603632ee1fe95e3812f00f752f70f7244c
Theo Paris <theoparisdesigns@gmail.com>
2021-07-07 16:22:15 -0700

    fix: prettier config

--- src/rules/prettier.js
+++ src/rules/prettier.js
@@ -1,15 +1,17 @@
 module.exports = {
     extends: ["prettier"],
     plugins: ["prettier"],
-    "prettier/prettier": [
-        "error",
-        {
-            singleQuote: false,
-            tabWidth: 4,
-            printWidth: 80,
-            semi: true,
-            trailingComma: "none",
-            endOfLine: "auto",
-        },
-    ],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                singleQuote: false,
+                tabWidth: 4,
+                printWidth: 80,
+                semi: true,
+                trailingComma: "none",
+                endOfLine: "auto",
+            },
+        ],
+    },
 };
